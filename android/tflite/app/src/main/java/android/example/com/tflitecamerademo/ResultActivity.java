bd3264cfc2479ee65200ecb76964041d1b1c44e5
bloshchytska <anastasiia.bloshchytska@comsysto.com>
2018-11-17 11:30:29 +0100

    added text files, updated result view, removed result class

--- android/tflite/app/src/main/java/android/example/com/tflitecamerademo/ResultActivity.java
+++ android/tflite/app/src/main/java/android/example/com/tflitecamerademo/ResultActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.hardware.camera2.CameraAccessException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.android.tflitecamerademo.CameraActivity;
 import com.example.android.tflitecamerademo.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import static com.example.android.tflitecamerademo.CameraActivity.sCustomFont;
 
@@ -35,13 +40,40 @@ public class ResultActivity extends Activity {
 
         if (extras != null) {
             String storyNameText = extras.getString("STORY_NAME").toUpperCase();
-            Resultmodel resultModel = new Resultmodel();
+            String story = readTextFile(extras.getString("STORY_NAME"));
+            storyTextView.setText(story);
 
-            storyTextView.setText(resultModel.getTextFor(extras.getString("STORY_NAME")));
             textViewStoryName.setText(storyNameText);
         } else {
             textViewStoryName.setText("???");
         }
     }
 
+    private String readTextFile(String name) {
+
+        BufferedReader reader = null;
+        String textFromFile = "";
+        String textFileName = "text/"+ name + ".txt";
+
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("text/balder.txt"), "UTF-8"));
+
+            String mLine = "";
+            while ((mLine = reader.readLine()) != null) {
+                textFromFile += mLine;
+            }
+        } catch (IOException e) {
+            Log.e("exception", e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Log.e("exception by closing", e.getMessage());
+                }
+            }
+        }
+        return textFromFile;
+    }
 }
